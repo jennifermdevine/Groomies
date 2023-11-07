c03a92f2f260558c54bc54d20c1cc272abccbdc7
Jam19th <jam19th@gmail.com>
2023-11-06 18:42:40 -0800

    added in boilerplate registration

--- groomies/src/components/Register.jsx
+++ groomies/src/components/Register.jsx
@@ -1,1 +1,20 @@
-import { supabase } from "../supabaseClient";
+import { Auth } from '@supabase/auth-ui-react'
+import {
+    // Import predefined theme
+    ThemeSupa,
+} from '@supabase/auth-ui-shared'
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(
+    process.env.REACT_APP_SUPABASE_URL,
+    process.env.REACT_APP_ANON_KEY
+)
+
+const Register = () => (
+    <Auth
+        supabaseClient={supabase}
+        appearance={{ theme: ThemeSupa }}
+    />
+)
+
+export default Register
