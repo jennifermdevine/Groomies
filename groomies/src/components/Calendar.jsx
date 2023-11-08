3a019927c44f65c85ecaa831d9da8a85382a1512
Zoren Villareal <zoren.villareal@gmail.com>
2023-11-07 21:27:20 -0800

    calendar updated

--- groomies/src/components/Calendar.jsx
+++ groomies/src/components/Calendar.jsx
@@ -41,6 +41,31 @@ export default function Calendar() {
         <Fullcalendar
         plugins={[dayGridPlugin,timeGridPlugin,interactionPlugin]}
         initialView ={"dayGridMonth"}
+        headerToolbar= {{
+            start: 'today prev,next',
+            center: 'title,addEventButton',
+            end:  'dayGridMonth, timeGridWeek, timeGridDay'
+        }}
+        customButtons= {{
+            addEventButton: {
+              text: 'add event...',
+              click: function() {
+                var dateStr = prompt('Enter a date in YYYY-MM-DD format');
+                var date = new Date(dateStr + 'T00:00:00'); // will be in local time
+
+                if (!isNaN(date.valueOf())) { // valid?
+                  Fullcalendar.addEvent({
+                    title: 'dynamic event',
+                    start: date,
+                    allDay: true
+                  });
+                  alert('Great. Now, update your database...');
+                } else {
+                  alert('Invalid date.');
+                }
+              }
+            }
+          }}
         />
     </div>
     );
