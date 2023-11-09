e0883e6c85ba1f65350cc47fd5870432c1175cda
Jennifer <jennifermdevine88@gmail.com>
2023-11-09 12:24:30 -0800

    css work x2

--- groomies/src/components/Nav.jsx
+++ groomies/src/components/Nav.jsx
@@ -17,7 +17,7 @@ export default function Nav() {
     };
 
     return (
-        <nav className="flex items-center justify-between flex-wrap bg-gray-800 p-6">
+        <nav className="nav">
             <div className="flex items-center flex-shrink-0 text-white mr-6">
                 <Link to="/">
                     <span className="font-semibold text-xl tracking-tight">
