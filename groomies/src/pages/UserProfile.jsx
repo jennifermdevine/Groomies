d249a14fe0d79fbc970f763d70be6049f688e572
Jam19th <jam19th@gmail.com>
2023-11-10 19:38:15 -0800

    userprofile now displays current users data

--- groomies/src/pages/UserProfile.jsx
+++ groomies/src/pages/UserProfile.jsx
@@ -1,143 +1,112 @@
-// UserProfile.jsx
-import { useEffect, useReducer, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useReducer, useState } from 'react';
 import { Helmet } from "react-helmet-async";
-import { fetchPetsWithImages, getImageUrl } from './PetProfile';
-import { supabase } from '../supabaseClient';
 import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
+import { useUser } from '../components/UserContext';
 
+// Reducer for managing the state
 const reducer = (state, action) => {
+  console.log("Reducer action received:", action);
   switch (action.type) {
     case 'FETCH_REQUEST':
-      return { ...state, loading: true, error: "" };
+      return { ...state, loading: true, error: '' };
     case 'FETCH_SUCCESS':
       return { ...state, user: action.payload, loading: false, error: '' };
     case 'FETCH_FAIL':
       return { ...state, loading: false, error: action.payload };
-    case 'UPDATE_USER':
-      return { ...state, user: { ...state.user, ...action.payload } };
-    case 'TOGGLE_EDIT':
-      return { ...state, isEditing: !state.isEditing };
     default:
       return state;
   }
 };
 
 const initialState = {
-  user: {
-    pets: [],
-  },
+  user: null,
   loading: false,
   error: '',
-  isEditing: false,
 };
 
 export default function UserProfile() {
-  const { userId } = useParams();
-  const [userImage, setUserImage] = useState(null);
-
+  const { user: contextUser } = useUser();
   const [{ user, loading, error }, dispatch] = useReducer(reducer, initialState);
-  
+  const [userImage, setUserImage] = useState();
 
   useEffect(() => {
-    const fetchUsers = async () => {
-      const loggedInUser = supabase.auth.user;
-
-      if (!loggedInUser) {
-        return;
-      }
-
+    if (contextUser) {
       dispatch({ type: 'FETCH_REQUEST' });
-      const { data, error } = await supabase
-        .from('users')
-        .select(`
-          *,
-          pets!inner(petId, petName, petSlug, petImage, species )
-        `)
-        .eq('userId', userId);
-
-      if (error) {
-        console.error('Error fetching user:', error);
-        dispatch({ type: 'FETCH_FAIL', payload: error.message });
-        return;
+      try {
+        // If you have a specific logic to fetch the user image or additional data, apply it here
+        const imageUrl = contextUser.userImage || 'default_profile_image.jpg';
+        setUserImage(imageUrl);
+        dispatch({ type: 'FETCH_SUCCESS', payload: contextUser });
+      } catch (error) {
+        dispatch({ type: 'FETCH_FAIL', payload: 'Failed to fetch user data' });
       }
-
-      if (data && data.length > 0) {
-        const user = data[0];
-        const petsArray = user.pets ? (Array.isArray(user.pets) ? user.pets : [user.pets]) : [];
-        const petsWithImages = await fetchPetsWithImages(petsArray);
-        // console.log('petsWithImages:', petsWithImages);
-        dispatch({ type: 'FETCH_SUCCESS', payload: { ...user, pets: petsWithImages } });
-      } else {
-        dispatch({ type: 'FETCH_FAIL', payload: 'No user found' });
-      }
-    };
-
-    fetchUsers();
-  }, [userId]);
-
-  useEffect(() => {
-    const fetchUserImage = async () => {
-      if (user?.userImage) {
-        try {
-          const url = await getImageUrl('users', user.userImage);
-          setUserImage(url);
-        } catch (error) {
-          console.error('Error fetching user image:', error);
-        }
-      }
-    };
-
-    fetchUserImage();
-  }, [user?.userImage]);
+    } else {
+      dispatch({ type: 'FETCH_FAIL', payload: 'User not logged in' });
+    }
+  }, [contextUser]);
 
   return (
     <div>
       <Helmet>
-        <title>{user.userSlug ? `${user.userSlug}'s Profile` : 'User Profile'}</title>
+      <title>{user?.userSlug ? `${user.userSlug}'s Profile` : 'User Profile'}</title>
       </Helmet>
-      <h1>User Profile:</h1>
+      <h1 style={{color: 'rgb(17, 28, 52)', fontWeight: '800'}}>User Profile</h1>
       {loading && <p>Loading...</p>}
       {error && <p>Error: {error}</p>}
-      {user.userName && (
-        <div>
-          <img
+      {user && (
+        <div className="profWrapper">
+        <Card>
+        <Card.Title>{user.fullName}</Card.Title>
+          <Card.Body>
+          <Card.Img
+            className="profImg"
+            variant="top"
             src={userImage || 'default_profile_image.jpg'}
             alt={`${user.userName}'s profile`}
             style={{
+              height: '20vh',
+              width: 'calc(50vw / 3)',
               objectFit: 'cover'
             }}
-          />
-          <h2>Name: {user.fullName}</h2>
-          <p>Email: {user.email}</p>
-          <p>Username: {user.userName}</p>
-          <div>
-            <h3>Pets:</h3>
+          /> 
+            
+            <Card.Text>{user.email}</Card.Text>
+            <Card.Text>Username: {user.userName}</Card.Text>
+            </Card.Body>
+            </Card>
+            <div>
+            
             {user.pets && user.pets.length > 0 ? (
               user.pets.map(pet => (
-                <div key={pet.petId}>
-                  <p>Name: {pet.petName}</p>
-                  <p>Species: {pet.species}</p>
-                  {pet.imageUrl && (
-                    <img
-                      src={pet.imageUrl}
-                      alt={`${pet.petName}`}
-                      style={{
-                        height: '20vh',
-                        width: 'calc(50vw / 3)',
-                        objectFit: 'cover'
-                      }}
-                    />
-                  )}
-                </div>
+                <Card key={pet.petId} className="profCards">
+                  <Card.Body>
+                    <Card.Text>Name: {pet.petName}</Card.Text>
+                    <Card.Text>Species: {pet.species}</Card.Text>
+                    {pet.imageUrl && (
+                      <Card.Img
+                        className="profImg"
+                        variant="bottom"
+                        src={pet.imageUrl}
+                        alt={`${pet.petName}`}
+                        style={{
+                          height: '20vh',
+                          width: 'calc(50vw / 3)',
+                          objectFit: 'cover'
+                        }}
+                      />
+                      
+                    )}
+                  </Card.Body>
+                </Card>
+                
               ))
             ) : (
               <p>No pets found.</p>
             )}
+            </div>
           </div>
-        </div>
       )}
-    </div>
+      </div>
+      
   );
 }
