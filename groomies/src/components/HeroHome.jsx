857ce19a96768e24b1a17c65f41297bfdc414181
Francisco Menendez <fmenendezwork@gmail.com>
2023-11-12 03:09:58 -0800

    minor changes HeroHome styling

--- groomies/src/components/HeroHome.jsx
+++ groomies/src/components/HeroHome.jsx
@@ -12,8 +12,20 @@ function HeroHome() {
             alt="first slide"
           />
           <Carousel.Caption className="custom-caption">
-            <h1>First slide label</h1>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            <h1>
+              Mobile Grooming at Your Doorstep: Tailored Pet Care, Effortless
+              Grooming
+            </h1>
+            <p>
+              Welcome to Groomies, where convenience meets care. We're dedicated
+              to delivering top-notch grooming services right to your doorstep,
+              ensuring your pets look and feel their best without the hassle of
+              leaving your home. Whether you're a pet owner looking for
+              stress-free grooming or a talented groomer seeking opportunities
+              to shine, our platform is the ultimate destination for tailored
+              pet care and thriving careers. Join us in creating a world where
+              happy pets and passionate groomers come together seamlessly!
+            </p>
           </Carousel.Caption>
         </Carousel.Item>
 
