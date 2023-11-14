4b78175fa25798a20401d382025a8d4fa1b94c6d
Jam19th <jam19th@gmail.com>
2023-11-13 16:46:54 -0800

    separated the calendar and modal into their own files

--- groomies/src/components/Calendar.jsx
+++ groomies/src/components/Calendar.jsx
@@ -1,80 +1,25 @@
+// Calendar.jsx
 import React, { useState, useEffect } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
-import Modal from 'react-modal';
-import TimePicker from 'react-time-picker';
 import { supabase } from '../supabaseClient';
-import '../App.css';
-import './Calendar.css';
 import { useUser } from './UserContext';
 import { useNavigate } from 'react-router-dom';
+import CalendarModal from './CalendarModal';
 import { Helmet } from "react-helmet-async";
+import Modal from 'react-modal';
+import '../App.css';
+import './Calendar.css';
 
 Modal.setAppElement('#root');
 
 export default function Calendar() {
   const [events, setEvents] = useState([]);
   const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [appointmentDate, setAppointmentDate] = useState('');
-  const [appointmentTitle, setAppointmentTitle] = useState('');
-  const [appointmentTime, setAppointmentTime] = useState('10:00');
-  const [pets, setPets] = useState([]);
-  const [selectedPet, setSelectedPet] = useState('');
-  const [groomies, setGroomies] = useState([]);
-  const [selectedGroomie, setSelectedGroomie] = useState('');
-  const navigate = useNavigate();
   const { user } = useUser();
-
-  useEffect(() => {
-    fetchAppointments();
-    fetchGroomies();
-    if (user?.userId) {
-      fetchUserPets(user.userId);
-    }
-  }, [user]);
-
-  const fetchGroomies = async () => {
-    try {
-      const { data, error } = await supabase
-        .from('groomies')
-        .select('groomieId, groomieName');
-
-      if (error) {
-        console.error('Error fetching groomies:', error);
-      } else {
-        setGroomies(data);
-      }
-    } catch (error) {
-      console.error('Error fetching groomies:', error);
-    }
-  };
-
-  const handleGroomieChange = (e) => {
-    setSelectedGroomie(e.target.value);
-  };
-
-  const fetchUserPets = async (userId) => {
-    try {
-      const { data, error } = await supabase
-        .from('pets')
-        .select('petId, petName')
-        .eq('userId', userId);
-
-      if (error) {
-        console.error('Error fetching pets:', error);
-      } else {
-        setPets(data);
-      }
-    } catch (error) {
-      console.error('Error fetching pets:', error);
-    }
-  };
-
-  const handlePetChange = (e) => {
-    setSelectedPet(e.target.value);
-  };
+  const navigate = useNavigate();
 
   const fetchAppointments = async () => {
     const { data, error } = await supabase
@@ -85,7 +30,7 @@ export default function Calendar() {
       console.error('Error fetching appointments:', error);
     } else {
       const fetchedEvents = data.map(appt => ({
-        id: appt.appointmentId, // Set the event ID to the appointmentId from your database
+        id: appt.appointmentId,
         title: appt.title,
         start: new Date(appt.appointment).toISOString(),
         allDay: false,
@@ -94,43 +39,18 @@ export default function Calendar() {
     }
   };
 
-  const addAppointment = async (dateTime, title, userId, petId, groomieId) => {
-    const { error } = await supabase
-      .from('appointments')
-      .insert([
-        {
-          appointment: new Date(dateTime).toISOString(),
-          title,
-          userId,
-          petId,
-          groomieId
-        }
-      ]);
-
-    if (error) {
-      console.error('Error adding new appointment:', error);
-    } else {
-      fetchAppointments();
-    }
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const dateTime = `${appointmentDate}T${appointmentTime}`;
-    addAppointment(dateTime, appointmentTitle, user.userId, selectedPet, selectedGroomie);
-    setModalIsOpen(false);
-  };
-
   useEffect(() => {
     fetchAppointments();
-    fetchGroomies();
   }, []);
 
   const handleEventClick = ({ event }) => {
     if (user) {
-      // Navigate to the Appointments page with the specific appointmentId
-      navigate(`/appointment/${event.id}`); // Use the event.id which is the appointmentId
+      navigate(`/appointment/${event.id}`);
     }
+  };
+
+  const openModal = () => {
+    setModalIsOpen(true);
   };
 
   return (
@@ -164,77 +84,12 @@ export default function Calendar() {
           slotMaxTime="18:00:00"
           eventClick={handleEventClick}
         />
-        <Modal
+        <CalendarModal
           isOpen={modalIsOpen}
-          onRequestClose={() => setModalIsOpen(false)}
-          contentLabel="Add Appointment"
-          overlayClassName="react-modal-overlay"
-          className="react-modal-content"
-        >
-          <h2>Make a New Appointment</h2>
-          <form onSubmit={handleSubmit}>
-            <div>
-              <label>
-                Appointment Title:
-                <input
-                  type="text"
-                  value={appointmentTitle}
-                  onChange={(e) => setAppointmentTitle(e.target.value)}
-                />
-              </label>
-            </div>
-            <div>
-              <label>
-                Appointment Date:
-                <input
-                  type="date"
-                  value={appointmentDate}
-                  onChange={(e) => setAppointmentDate(e.target.value)}
-                />
-              </label>
-            </div>
-            <div>
-              <label>
-                Appointment Time:
-                <div className="custom-time-picker">
-                  <TimePicker
-                    onChange={setAppointmentTime}
-                    value={appointmentTime}
-                    format="HH:mm"
-                    hourPlaceholder="hh"
-                    minutePlaceholder="mm"
-                    step={30}
-                  />
-                </div>
-              </label>
-            </div>
-            <div>
-              <label>
-                Select Pet:
-                <select value={selectedPet} onChange={handlePetChange}>
-                  {pets.map(pet => (
-                    <option key={pet.petId} value={pet.petId}>{pet.petName}</option>
-                  ))}
-                </select>
-              </label>
-            </div>
-            <div>
-              <label>
-                Select Groomie:
-                <select value={selectedGroomie} onChange={handleGroomieChange}>
-                  <option value="">Select a Groomie</option>
-                  {groomies.map(groomie => (
-                    <option key={groomie.groomieId} value={groomie.groomieId}>
-                      {groomie.groomieName}
-                    </option>
-                  ))}
-                </select>
-              </label>
-            </div>
-            <button type="submit">Add Appointment</button>
-            <button type="button" onClick={() => setModalIsOpen(false)}>Cancel</button>
-          </form>
-        </Modal>
+          onClose={() => setModalIsOpen(false)}
+          fetchAppointments={fetchAppointments}
+          user={user}
+        />
       </div>
     </div>
   );
